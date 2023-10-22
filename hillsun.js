dafebeaeda2a39bc859e431e585902f12d55db96
Vikoadrian <148675486+Vikoadrian@users.noreply.github.com>
2023-10-22 15:04:42 +0700

    testing

---
+++ hillsun.js
@@ -0,0 +1,21 @@
+document.getElementById("klikbutton").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
+document.getElementById("klikbutton").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
+document.getElementById("klikbutton1").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
+document.getElementById("klikbutton2").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
+document.getElementById("klikbutton3").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
+document.getElementById("klikbutton4").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
+document.getElementById("klikbutton5").addEventListener("click",function(){
+    window.location.href = "https://wa.me/qr/WBQBINHUNQT4L1"
+});
